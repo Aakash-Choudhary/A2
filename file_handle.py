5d80283d42ff16471c3c4d1ac9bda34665dd8e78
NagRaaz <priyavardhan09032000@gmail.com>
2019-06-12 04:35:01 +0000

    file handle

---
+++ file_handle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import os
+
+
+
+"""
+f= open("ak.txt","w")
+f.write("oops!!! i just removed all the content")
+f.close()
+
+
+f=open("ak.txt","a")
+f.write("this is the new line added")
+f.close()
+
+f=open("ak.txt","x")
+f.close()
+
+f=open("ak.txt","a")
+f.write("i am adding this " "i am going to market" "i am writing anything")
+f.close() 
+
+
+f= open("ak.txt","r")
+for x in f:
+	print(x)
+
+f.close()
+"""
+os.remove("ak.txt")
+
